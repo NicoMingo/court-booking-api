a0f133dbd2b9641b31c72997ae5f9caab3b21fa2
NicoMingo <142063764+NicoMingo@users.noreply.github.com>
2026-03-10 23:00:19 -0300

    Update database.py

--- app/database.py
+++ app/database.py
@@ -4,14 +4,9 @@ from sqlalchemy.orm import sessionmaker
 import os
 from dotenv import load_dotenv
 
-# Esto busca tu archivo .env y lo carga en la memoria
 load_dotenv() 
 
-# Ahora sí, Python puede encontrar la variable secreta
 SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL")
-
-# Acá abajo debería seguir tu código normal...
-# engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
